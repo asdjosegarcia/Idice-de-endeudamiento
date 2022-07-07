0775c9740ff813d6ea4fe3ff0c59e260783f2e04
asdjosegarcia <asdjosegarcia@gmail.com>
2022-07-07 03:41:45 -0300

    v0.1 website mobile completed

--- indice-endeudamiento.js
+++ indice-endeudamiento.js
@@ -1,4 +1,8 @@
-totalActivos=3597000
-totalPasivos=1643000
-indiceEndeudamiento=(totalPasivos/totalActivos)*100
-console.log(indiceEndeudamiento +'%');
+function calcularEndeudamiento() {
+    const totalActivos = document.getElementById("input-activo").value;
+    const totalPasivos = document.getElementById("input-pasivo").value;
+    /* totalActivos = 3597000
+    totalPasivos = 1643000 */
+    indiceEndeudamiento = ((totalPasivos / totalActivos) * 100).toFixed(2)
+    resultado.innerText = indiceEndeudamiento +" %";
+}
